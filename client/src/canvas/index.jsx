fb5426fcd72ad239f375442e0d23d0a3c9b5b794
Ekansh Chawla <exchawla@gmail.com>
2024-10-31 16:53:12 -0700

    fixed export and Modified index.jsx to include all components - INFO: STILL BUGGY

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import {Canvas} from '@react-three/fiber'
-import {Enviornment, Center} from '@react-three/drei'
-
+import {Environment, Center} from '@react-three/drei'
+import Shirt from './Shirt'
+import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
 
 const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+        <ambientLight intensity={0.5} />
+        <Environment preset=' city'/>
+        {/* <CameraRig>
+        <Backdrop/> */}
+        <Center>
+            <Shirt/>
+        </Center>
+        {/* </CameraRig> */}
+
+    </Canvas>
   )
 }
 
